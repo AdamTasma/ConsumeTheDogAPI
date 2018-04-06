1124c4c0239614af69457c8a8f79c8773bc9cea3
Adam Tasma <adamtasma@gmail.com>
2018-04-05 20:52:08 -0400

    first commit

---
+++ ConsumeTheDogAPI/Case1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ConsumeTheDogAPI
+{
+    class Case1 //Print all dog breeds
+    {
+        public static void Write()
+        {
+            Console.WriteLine("from case 1 write method.");
+        }
+
+        public static void PrintAllBreeds()
+        {
+            HttpWebRequest request = WebRequest.CreateHttp("https://dog.ceo/api/breeds/list/all");
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            String data = rd.ReadToEnd();
+            JObject o = JObject.Parse(data);
+            //Console.WriteLine(o);
+            JToken breedList = o["message"];
+
+        }
+    }
+}
---
+++ ConsumeTheDogAPI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsumeTheDogAPI
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the dog consumer!");
+            ShowList();
+            Console.ReadLine();
+        }
+
+        static void ShowList()
+        {
+            Console.WriteLine("What would you like to do? (Please type a numeric number and hit enter)");
+            Console.WriteLine("1) Print all dog breeds." +
+                            "\n2) Download and save a random dog image." +
+                            "\n3) Print all sub-breeds of a user specified breed." +
+                            "\n4) Download and save an image of a user specified breed." +
+                            "\n");
+            if (int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine("case 1");
+                        Case1.PrintAllBreeds();
+                        break;
+                    case 2:
+                        Console.WriteLine("case 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("case 3");
+                        break;
+                    default:
+                        Console.WriteLine("{0} is not a valid option.",userInput);
+                        ShowList();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number, for example '2'.");
+                ShowList();
+            }
+        }
+    }
+}
